d9d1462fbe2b8e432dd46619f839735a074b6b81
wolfgang <wmhuber2@gmail.com>
2023-03-19 02:15:41 -0500

    minor bug changes in display

--- data/displayChannels.py
+++ data/displayChannels.py
@@ -197,7 +197,8 @@ async def updateData(self):
                   'Suber-Votes-3','Suber-Votes-4','Suber-Votes-5','Subers',
                   'Suber-MSG','Votes','Mood','Array','lastAlive','NextTurnStartTime',
                   'CurrTurnStartTime','channels', 'admin','server',
-                  'Turn -3 RPS Winners','Turn -2 RPS Winners','Turn -1 RPS Winners']
+                  'Turn -3 RPS Winners','Turn -2 RPS Winners','Turn -1 RPS Winners',
+                  'Haymaker','VotingEnabled','Votes-1','Votes-2','Votes-3','Votes-4','Votes-5','Wizard']
     paramMap = {
         'TurnTime': 'Elasped Turn Time (sec)'
     }
